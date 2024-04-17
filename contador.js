a480b114665a842dd9a0c030427eb32800fe012c
Gabriela Gonçalves <97477370+gabrielacgc@users.noreply.github.com>
2024-04-17 09:06:28 -0300

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,11 @@
+let valor = 1;
+
+function inc() { 
+    document.getElementById('contador').innerHTML = ++valor; 
+
+}
+
+function dec() { 
+    document.getElementById('contador').innerHTML = --valor; 
+    
+}
